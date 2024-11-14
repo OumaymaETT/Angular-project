d748e0e0506c373c8de6b3cd11babc858583540e
OumaymaETT <ettayyeboumayma2222@gmail.com>
2024-11-14 19:16:35 +0100

    EX1-EX2-EX3

--- src/app/exercise3/exercise3.component.ts
+++ src/app/exercise3/exercise3.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-exercise3',
   templateUrl: './exercise3.component.html',
   styleUrls: ['./exercise3.component.css']
 })
-export class Exercise3Component implements OnInit {
+export class Exercise3Component  {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  goToTransactions() {
+    this.router.navigate(['/transactions']);
   }
 
 }
